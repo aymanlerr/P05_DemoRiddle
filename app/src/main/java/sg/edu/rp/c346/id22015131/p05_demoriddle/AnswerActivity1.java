dc96f70b8c529c5e1db1efb3c45bd92e5e21f99f
22015131 <22015131@myrp.edu.sg>
2023-05-23 15:10:01 +0800

    section C

--- app/src/main/java/sg/edu/rp/c346/id22015131/p05_demoriddle/AnswerActivity1.java
+++ app/src/main/java/sg/edu/rp/c346/id22015131/p05_demoriddle/AnswerActivity1.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -16,6 +17,7 @@ public class AnswerActivity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer1);
+        Log.d("AnswerActivity1", "onCreate() called.");
 
         tvAnswer = findViewById(R.id.textView);
         tvAnswer.setText("In Second Activity");
@@ -23,4 +25,15 @@ public class AnswerActivity1 extends AppCompatActivity {
         String questionsSelected = intentReceived.getStringExtra("Question");
         tvAnswer.setText(questionsSelected + " answer is: Queue");
     }
+    @Override
+    protected void onStart() {
+        Log.d("AnswerActivity1", "onStart() called.");
+        super.onStart();
+    }
+    @Override
+    protected void onResume() {
+        Log.d("AnswerActivity1", "onResume() called.");
+        super.onResume();
+    }
+
 }
